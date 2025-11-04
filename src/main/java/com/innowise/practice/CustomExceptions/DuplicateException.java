97a3b468e8c55c75006b5893fc4dbf88886d4666
xiakuu <100316445+xiakuu@users.noreply.github.com>
2025-11-05 02:12:40 +0300

    controller layer

---
+++ src/main/java/com/innowise/practice/CustomExceptions/DuplicateException.java
@@ -0,0 +1,7 @@
+package com.innowise.practice.CustomExceptions;
+
+public class DuplicateException extends RuntimeException {
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
