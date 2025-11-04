aa70818b3f275bc29e398514b3a2ae413d1e5e66
xiakuu <100316445+xiakuu@users.noreply.github.com>
2025-11-05 02:01:47 +0300

    DBSchema task

---
+++ src/main/java/com/innowise/practice/DBSchema/CardInfo.java
@@ -0,0 +1,32 @@
+package com.innowise.practice.DBSchema;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "card_info")
+public class CardInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    private String number;
+    private String holder;
+    @Column(name = "expiration_date")
+    private LocalDate expiration_date;
+}
