0e5553554d3a7a4e2d9a032ccd1edc0e5fd62baf
xiakuu <100316445+xiakuu@users.noreply.github.com>
2025-12-01 03:53:03 +0300

    Delete src/main/java/com/innowise/orderservice/orderservice/repository directory

--- src/main/java/com/innowise/orderservice/orderservice/repository/OrderItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.innowise.orderservice.orderservice.repository;
-
-import com.innowise.orderservice.orderservice.model.OrderItems;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemsRepository extends JpaRepository<OrderItems, Long> {
-}
