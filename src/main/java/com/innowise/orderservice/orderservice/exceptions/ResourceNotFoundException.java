c4dae710b24c8896d0e1c6889008151797d8c75c
xiakuu <100316445+xiakuu@users.noreply.github.com>
2026-01-14 05:03:56 +0300

    delete all

--- src/main/java/com/innowise/orderservice/orderservice/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.innowise.orderservice.orderservice.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
